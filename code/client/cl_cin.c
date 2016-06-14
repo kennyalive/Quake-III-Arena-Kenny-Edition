0d47a2c605a8fb9b1cf662993fe56c9c978742bb
Artem Kharytoniuk <artemalive@gmail.com>
2016-06-14 23:17:44 +0300

    Fixed pointer conversion warning on x64. Code is replaced with functionally equivalent code.

--- code/client/cl_cin.c
+++ code/client/cl_cin.c
@@ -1050,11 +1050,11 @@ static void readQuadInfo( byte *qData )
 	cinTable[currentHandle].VQ0 = cinTable[currentHandle].VQNormal;
 	cinTable[currentHandle].VQ1 = cinTable[currentHandle].VQBuffer;
 
-	cinTable[currentHandle].t[0] = (0 - (unsigned int)cin.linbuf)+(unsigned int)cin.linbuf+cinTable[currentHandle].screenDelta;
-	cinTable[currentHandle].t[1] = (0 - ((unsigned int)cin.linbuf + cinTable[currentHandle].screenDelta))+(unsigned int)cin.linbuf;
+    cinTable[currentHandle].t[0] = cinTable[currentHandle].screenDelta;
+    cinTable[currentHandle].t[1] = -cinTable[currentHandle].screenDelta;
 
-        cinTable[currentHandle].drawX = cinTable[currentHandle].CIN_WIDTH;
-        cinTable[currentHandle].drawY = cinTable[currentHandle].CIN_HEIGHT;
+    cinTable[currentHandle].drawX = cinTable[currentHandle].CIN_WIDTH;
+    cinTable[currentHandle].drawY = cinTable[currentHandle].CIN_HEIGHT;
 }
 
 /******************************************************************************
