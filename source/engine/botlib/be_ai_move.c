b03511ec2ca27f27002f166bf8ffa63859ae1a1e
Artem Kharytoniuk <artemalive@gmail.com>
2016-07-10 13:28:45 +0300

    Moved botlib project to source/engine/botlib.

--- source/engine/botlib/be_ai_move.c
+++ source/engine/botlib/be_ai_move.c
@@ -29,7 +29,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  *****************************************************************************/
 
-#include "../game/q_shared.h"
+#include "../../game/q_shared.h"
 #include "l_memory.h"
 #include "l_libvar.h"
 #include "l_utils.h"
@@ -37,14 +37,14 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "l_precomp.h"
 #include "l_struct.h"
 #include "aasfile.h"
-#include "../game/botlib.h"
-#include "../game/be_aas.h"
+#include "../../game/botlib.h"
+#include "../../game/be_aas.h"
 #include "be_aas_funcs.h"
 #include "be_interface.h"
 
-#include "../game/be_ea.h"
-#include "../game/be_ai_goal.h"
-#include "../game/be_ai_move.h"
+#include "../../game/be_ea.h"
+#include "../../game/be_ai_goal.h"
+#include "../../game/be_ai_move.h"
 
 
 //#define DEBUG_AI_MOVE
