a16c11adf6a4af03d1d05fcb1601941fe9d606ec
Artem Kharytoniuk <artemalive@gmail.com>
2017-03-17 20:43:33 +0200

    Disabled bot log.

--- src/engine/botlib/be_interface.c
+++ src/engine/botlib/be_interface.c
@@ -140,8 +140,8 @@ int Export_BotLibSetup(void)
 	bot_developer = LibVarGetValue("bot_developer");
   memset( &botlibglobals, 0, sizeof(botlibglobals) ); // bk001207 - init
 	//initialize byte swapping (litte endian etc.)
-//	Swap_Init();
-	Log_Open("botlib.log");
+    // Swap_Init();
+	// Log_Open("botlib.log");
 	//
 	botimport.Print(PRT_MESSAGE, "------- BotLib Initialization -------\n");
 	//
