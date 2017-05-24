a9431c5314314bba215f0095515b6d474aec5ec5
Artem Kharytoniuk <artemalive@gmail.com>
2017-05-24 19:32:33 +0300

    Fixed stencil shadows incorrect rendering in some scenarious. It's due to bug in render pass creation code  (changeable variable cannot be used in renderpass description).

--- src/engine/renderer/vk.cpp
+++ src/engine/renderer/vk.cpp
@@ -169,13 +169,12 @@ static VkRenderPass create_render_pass(VkDevice device, VkFormat color_format, V
     attachments[0].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
     attachments[0].finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
 
-	bool clear_stencil = (r_shadows->integer == 2);
     attachments[1].flags = 0;
 	attachments[1].format = depth_format;
     attachments[1].samples = VK_SAMPLE_COUNT_1_BIT;
     attachments[1].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
     attachments[1].storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-    attachments[1].stencilLoadOp = clear_stencil ? VK_ATTACHMENT_LOAD_OP_CLEAR : VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    attachments[1].stencilLoadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
     attachments[1].stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
     attachments[1].initialLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
     attachments[1].finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
